ddb354c671e840179e908d789aeee4bc7ec38b68
Daniel Callaghan <dan_cal89@hotmail.com>
2022-04-15 19:49:39 +0000

    add boilerplate allauth base html style

--- ebiblio/settings.py
+++ ebiblio/settings.py
@@ -76,7 +76,7 @@ ROOT_URLCONF = 'ebiblio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': [TEMPLATES_DIR, os.path.join(BASE_DIR, 'templates/allauth/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
