9f6cd5e1972ab1e38feab51651a4a9210b64c870
Daniel Callaghan <dan_cal89@hotmail.com>
2022-04-16 14:36:16 +0000

    add urls for category and product detail view

--- store/urls.py
+++ store/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from .views import (
     landing,
+    product_detail,
+    category_list,
 )
 
 app_name = 'store'
 
 urlpatterns = [
     path('', landing, name='landing'),
+    path('<slug:slug>/', product_detail, name='product_detail'),
+    path('shop/<slug:category_slug>/', category_list, name='category_list'),
 ]
-
