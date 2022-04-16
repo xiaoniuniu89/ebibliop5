a746be64de338c02eaa3b3e887e44c935d81a89b
Daniel Callaghan <dan_cal89@hotmail.com>
2022-04-16 14:35:49 +0000

    add view for category and product detail

--- store/views.py
+++ store/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 
 def landing(request):
     """ render stoe landing page """
     products = Product.products.all()
     return render(request, 'store/landing.html', {'products': products})
+
+def product_detail(request, slug):
+    """ render product detail page """
+    product = get_object_or_404(Product, slug=slug, in_stock=True)
+    return render(request, 'store/detail.html', {'product': product})
+
+def category_list(request, category_slug):
+    """ render list of books in a category """
+    category = get_object_or_404(Category, slug=category_slug)
+    products = Product.objects.filter(category=category)
+    return render(
+        request,
+        'store/category.html',
+        {'category': category, 'products': products})
