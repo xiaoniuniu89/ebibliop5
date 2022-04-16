6593039425908e112131035de573e5d4d011c3f2
Daniel Callaghan <dan_cal89@hotmail.com>
2022-04-16 13:57:22 +0000

    fix typo in storags to storages

--- ebiblio/settings.py
+++ ebiblio/settings.py
@@ -175,7 +175,7 @@ AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
-DEFAULT_FILE_STORAGE = 'storags.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 # all auth settings
