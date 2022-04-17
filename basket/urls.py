8f5b1add2cfcc95136e6ce31d911fdfa47282a78
Daniel Callaghan <dan_cal89@hotmail.com>
2022-04-17 07:54:18 +0000

    add delete from basket url

--- basket/urls.py
+++ basket/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from .views import (
     basket_summary,
-    basket_add
+    basket_add,
+    basket_delete
 )
 
 app_name = 'basket'
@@ -10,5 +11,5 @@ app_name = 'basket'
 urlpatterns = [
     path('', basket_summary, name='basket_summary'),
     path('add/', basket_add, name='basket_add'),
-
+    path('delete/', basket_delete, name='basket_delete'),
 ]
