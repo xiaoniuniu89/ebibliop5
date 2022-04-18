dd8725583d2dc71a93e4cf6171b7f96d7db65f2c
Daniel Callaghan <dan_cal89@hotmail.com>
2022-04-18 09:23:25 +0000

    add view to handle ajax request to create orders

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
+from basket.basket import Basket
+from .models import Order, OrderItem
+
 
 # Create your views here.
+def add_order(request):
+    basket = Basket(request)
+    if request.POST.get('action') == 'post':
+        user_id = request.user.id
+        order_key = request.POST.get('order_key')
+        basket_total = basket.get_total_price()
+
+        # check order exists already
+        if Order.objects.filter(order_key=order_key).exists():
+            pass
+        else:
+            order = Order.objects.create(
+                user_id=user_id,
+                full_name='name',
+                address1='add1',
+                address2='add2',
+                total_price=basket_total,
+                order_key=order_key
+            )
+            order_id = order.pk
+            for item in basket:
+                OrderItem.objects.create(
+                    order_id=order_id,
+                    product=item['product'],
+                    price=item['price']
+                )
+        response = JsonResponse({'success': 'Return Something'})
+        return response
