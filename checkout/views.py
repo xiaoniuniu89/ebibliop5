e445b57f337d01992f56108e8233a37c9b8d21ff
Daniel Callaghan <dan_cal89@hotmail.com>
2022-04-18 09:38:37 +0000

    add webhook to check for successful payment

--- checkout/views.py
+++ checkout/views.py
@@ -5,8 +5,10 @@ if os.path.isfile('env.py'):
 import stripe
 
 from basket.basket import Basket
+from orders.views import payment_confirmation
 
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
 if os.path.isfile('env.py'):
     import env
@@ -33,4 +35,29 @@ def checkout(request):
 
 def checkout_complete(request):
     """ redirect page after successful order """
+    basket = Basket(request)
+    basket.clear()
     return render(request, 'checkout/checkout_complete.html')
+
+
+@csrf_exempt
+def stripe_webhook(request):
+    payload = request.body
+    event = None
+
+    try:
+        event = stripe.Event.construct_from(
+            json.loads(payload), stripe.api_key
+        )
+    except ValueError as e:
+        print(e)
+        return HttpResponse(status=400)
+
+    # Handle the event
+    if event.type == 'payment_intent.succeeded':
+        payment_confirmation(event.data.object.client_secret)
+
+    else:
+        print('Unhandled event type {}'.format(event.type))
+
+    return HttpResponse(status=200)
