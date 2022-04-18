74b96da9cff4ee7f61885d5c2ccbf222e89686a5
Daniel Callaghan <dan_cal89@hotmail.com>
2022-04-18 16:32:24 +0000

    filter distinct order items to avoid multiple instances of same product

--- dashboard/context_processors.py
+++ dashboard/context_processors.py
@@ -1,5 +1,6 @@
 from orders.models import OrderItem
 
 def purchases(request):
-    purchases = OrderItem.objects.filter(customer=request.user)
+    purchases = OrderItem.objects.filter(
+        customer=request.user).values('product').distinct()
     return ({'purchases': purchases})
