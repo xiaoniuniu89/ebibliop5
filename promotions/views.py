6b6b4c17366c1d03143de86e94d8fb121106fb3f
Daniel Callaghan <dan_cal89@hotmail.com>
2022-04-27 08:37:22 +0000

    add logic to receive ajax request, send data to session data, return json response to be able to update template

--- promotions/views.py
+++ promotions/views.py
@@ -1,6 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils import timezone
+from django.views.decorators.http import require_POST
 
-# Create your views here.
+from .models import Promo
+from .forms import PromoForm
+from django.http import JsonResponse
+from basket.basket import Basket
+import stripe
+import os
+
+stripe.api_key = os.environ.get('STRIPE_SECRET_KEY')
+# @require_POST
+# def promo(request):
+#     now = timezone.now()
+#     form = PromoForm(request.post)
+#     if form.is_valid():
+#         code = form.cleaned_data['code']
+#         try:
+#             promo_code = Promo.objects.get(
+#                 code__iexact=code,
+#             )
+#             request.session['promo_id'] = promo_code.id
+#         except Promo.DoesNotExist:
+#             request.session['promo_id'] = None
+#     return redirect('checkout:checout')
 
 def promo(request):
-    return render(request, 'store/landing.html')
+    basket = Basket(request)
+    if request.POST.get('action') == 'post':
+        promo_code = request.POST.get('promo_code')
+        promo_code = Promo.objects.get(code=promo_code)
+        basket.add_promo(promo_code.discount)
+        discount = basket.get_discount()
+        total = basket.get_total_price_after_discount()
+        response = JsonResponse({'code': basket.promo, 'discount': discount, 'total': total})
+        return response
