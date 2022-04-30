701b200853961b97851d85f88d68ba9446f4a213
Daniel Callaghan <dan_cal89@hotmail.com>
2022-04-30 19:24:59 +0000

    fix bug with filtering for unique user review, previously if user had no review on a book the product detail page would throw a 404 error, fixed with a try except block in the product detail view

--- store/views.py
+++ store/views.py
@@ -13,7 +13,10 @@ def product_detail(request, slug):
     """ render product detail page """
     product = get_object_or_404(Product, slug=slug, in_stock=True)
     reviews = Review.objects.filter(product=product).exclude(user=request.user)
-    user_review = get_object_or_404(Review, user=request.user, product=product)
+    try:
+        user_review = Review.objects.filter(user=request.user, product=product)[0]
+    except IndexError:
+        user_review = None
     return render(
         request,
         'store/detail.html',
