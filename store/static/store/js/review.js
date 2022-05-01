e4af9e02c5b8a99838fadcd9df0527f103d2f368
Daniel Callaghan <dan_cal89@hotmail.com>
2022-05-01 15:02:41 +0000

    add background to users own comments - update js rendering user comments to include updated id names

--- store/static/store/js/review.js
+++ store/static/store/js/review.js
@@ -1,15 +1,15 @@
 const addReview = (user, review, rating) => {
     document.querySelector('#add-review-modal-btn').disabled = true
     document.querySelector('.temp-review-wrapper').innerHTML = `
-<div class="media row  text-muted py-3 col-9">
+<div class="media row  text-muted py-3 col-12 bg-light border-top">
     <strong class="d-block text-gray-dark">@${user} - </strong>
     <i class="fas fa-star mx-1 "><p class="c-rate">${rating}</p></i>
     <div class="edit-delete-comment">
-        <i data-toggle="modal" data-target="#delete-review-modal" id="edit-review-btn" class="fas px-2 fa-edit"></i>
+        <i data-toggle="modal" data-target="#update-review-modal" id="edit-review-btn" class="fas px-2 fa-edit"></i>
         <i data-toggle="modal" data-target="#delete-review-modal" id="delete-review-btn" class="fas fa-trash-alt"></i>
     </div>  
 </div>
-<div id="temp-review" style="white-space: pre-wrap;" class="row new-line border-bottom pb-3 mb-0 border-gray">${review}</div>
+<div id="temp-review" style="white-space: pre-wrap;" class="row new-line border-bottom pb-3 mb-0 border-gray bg-light col-12">${review}</div>
 `
 }
 
@@ -21,7 +21,7 @@ const updateReview = (user, review, rating) => {
         }
     }
     document.querySelector('.temp-review-wrapper').innerHTML = `
-<div class="media row  text-muted py-3 col-9">
+<div class="media row  text-muted py-3 col-12 bg-light border-top">
     <strong class="d-block text-gray-dark">@${user} - </strong>
     <i class="fas fa-star mx-1 "><p class="c-rate">${rating}</p></i>
     <div class="edit-delete-comment">
@@ -29,7 +29,7 @@ const updateReview = (user, review, rating) => {
         <i data-toggle="modal" data-target="#delete-review-modal" id="delete-review-btn" class="fas fa-trash-alt"></i>
     </div>  
 </div>
-<div style="white-space: pre-wrap;" class="row new-line border-bottom pb-3 mb-0 border-gray">${review}</div>
+<div style="white-space: pre-wrap;" class="row new-line border-bottom pb-3 mb-0 border-gray bg-light col-12">${review}</div>
 `
 
 }
