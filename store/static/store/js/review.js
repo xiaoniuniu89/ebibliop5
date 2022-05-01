3510405872755ccc3eb31b1d20a992b974bb097d
Daniel Callaghan <dan_cal89@hotmail.com>
2022-05-01 13:46:57 +0000

    fix bug when deleting contents of a review on page when page already loaded

--- store/static/store/js/review.js
+++ store/static/store/js/review.js
@@ -5,20 +5,51 @@ const addReview = (user, review, rating) => {
     <strong class="d-block text-gray-dark">@${user} - </strong>
     <i class="fas fa-star mx-1 "><p class="c-rate">${rating}</p></i>
     <div class="edit-delete-comment">
-        <i id="edit-review-btn" class="fas px-2 fa-edit"></i>
-        <i data-toggle="modal" data-target="#delete_review_modal" id="delete-review-btn" class="fas fa-trash-alt"></i>
+        <i data-toggle="modal" data-target="#delete-review-modal" id="edit-review-btn" class="fas px-2 fa-edit"></i>
+        <i data-toggle="modal" data-target="#delete-review-modal" id="delete-review-btn" class="fas fa-trash-alt"></i>
+    </div>  
+</div>
+<div id="temp-review" style="white-space: pre-wrap;" class="row new-line border-bottom pb-3 mb-0 border-gray">${review}</div>
+`
+}
+
+const updateReview = (user, review, rating) => {
+    if(document.getElementById('user-review-info-wrapper')){
+
+        if(! document.getElementById('user-review-info-wrapper').innerHTML == ''){
+
+            document.getElementById('user-review-info-wrapper').innerHTML = ''
+            document.getElementById('user-review-body-wrapper').innerHTML = ''
+            document.getElementById('user-review-body-wrapper').style.border = 'none'
+            document.getElementById('temp-review').style.border = 'none'
+        }
+    }
+    document.querySelector('.temp-review-wrapper').innerHTML = `
+<div class="media row  text-muted py-3 col-9">
+    <strong class="d-block text-gray-dark">@${user} - </strong>
+    <i class="fas fa-star mx-1 "><p class="c-rate">${rating}</p></i>
+    <div class="edit-delete-comment">
+        <i data-toggle="modal" data-target="#delete-review-modal" id="edit-review-btn" class="fas px-2 fa-edit"></i>
+        <i data-toggle="modal" data-target="#delete-review-modal" id="delete-review-btn" class="fas fa-trash-alt"></i>
     </div>  
 </div>
 <div style="white-space: pre-wrap;" class="row new-line border-bottom pb-3 mb-0 border-gray">${review}</div>
 `
+
 }
 
 const deleteReview = () => {
-    if(document.getElementById('user-review-wrapper')){
-        document.getElementById('user-review-wrapper').innerHTML = ''
+    if(document.getElementById('user-review-info-wrapper')){
+        if(! document.getElementById('user-review-info-wrapper').innerHTML == ''){
+            document.getElementById('user-review-info-wrapper').innerHTML = ''
+
+        }else{
+        document.querySelector('.temp-review-wrapper').innerHTML = ''
+        document.querySelector('#add-review-modal-btn').disabled = false
+    }    
     }else{
         document.querySelector('.temp-review-wrapper').innerHTML = ''
+        document.querySelector('#add-review-modal-btn').disabled = false
     }
-    
     document.querySelector('#add-review-modal-btn').disabled = false
 }
