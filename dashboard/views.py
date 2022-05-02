deaa1e8c476ee8517ebacff1581f3579606ccaac
Daniel Callaghan <dan_cal89@hotmail.com>
2022-05-02 13:30:17 +0000

    change dashboard routes in dashboard view depending on user status

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,12 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
 
 from .models import Message
 
+
 def dashboard(request):
+    """ Depending on user status will render to a different part of the site"""
+    if request.user.is_superuser:
+        return redirect('admin:index')
+    elif request.user.is_staff:
+        return redirect("store_admin:index")
     return render(request, 'dashboard/dashboard.html')
 
+
 def send_message(request):
+    """ view to handle the contact form in the footer """
     if request.POST.get('action') == 'post':
         email = request.POST.get('email')
         content = request.POST.get('content')
