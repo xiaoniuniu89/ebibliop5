cb5a4d65ac3afc0f180c62d300363879ff36221a
Daniel Callaghan <dan_cal89@hotmail.com>
2022-05-03 22:28:55 +0000

    update newletter signal call. Using render_to_string shortcut solved all issues of rendering the mail properly with the newletter message body

--- promotions/signals.py
+++ promotions/signals.py
@@ -1,17 +1,10 @@
 from django.core.mail import EmailMessage
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.template import Context
-from django.template.loader import get_template
-from django.contrib.auth.models import User
-from django.core.mail import send_mail
-from django.conf import settings
-from django.template import Context
-from django.template.loader import get_template
-from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
-from django.utils.html import strip_tags
-from ebiblio.settings import EMAIL_HOST_USER
+from django.contrib.auth.models import User
+from django.conf import settings
+
 
 from .models import NewsLetter
 
@@ -25,11 +18,11 @@ def send_newsletter(sender, instance, **kwargs):
     users = [user.email for user in users]
     print(users)
     message = instance
-    message = get_template("newsletter.html").render({'message': message})
+    message = render_to_string("newsletter.html", {'message': message})
     mail = EmailMessage(
         subject="E-biblio Newsletter",
         body=message,
-        from_email=EMAIL_HOST_USER,
+        from_email=settings.EMAIL_HOST_USER,
         to=users,
     )
     mail.content_subtype = "html"
