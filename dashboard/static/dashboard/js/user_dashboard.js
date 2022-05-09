05af3d42e6bfd01ba0d6d931b0441843080d5894
Daniel Callaghan <dan_cal89@hotmail.com>
2022-05-09 09:31:28 +0000

    change event listener to specifically listen to dashboard tabs and not the tabs wrapper. update review title with hyperlink to review on the product detail page

--- dashboard/static/dashboard/js/user_dashboard.js
+++ dashboard/static/dashboard/js/user_dashboard.js
@@ -3,7 +3,8 @@ const dashContentDiv = document.querySelector('.dash-tab-content-wrapper')
 const contentDivs = document.querySelectorAll('.content-div')
 const dashTabs = document.querySelectorAll('.nav-link')
 
-dash.addEventListener('click', (e) => {
+dashTabs.forEach((tab) => {
+    tab.addEventListener('click', (e) => {
     dashTabs.forEach((tab) => {
         tab.classList.remove('active')
         e.target.classList.add('active')
@@ -14,4 +15,5 @@ dash.addEventListener('click', (e) => {
     })
     
     document.querySelector(`#${e.target.id}-div`).classList.add('show')
+    })
 })
